7ce171c62dfabb08bb74737f75e81bfab4fcea86
raysinensis <raysinensis@gmail.com>
2017-01-05 00:54:08 -0500

    variant to pull sequece info from NCBI

---
+++ 3primefold-web.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+import RNA ##install viennaRNA python binding first
+
+##for each gene, grab 3' most 100nt for folding
+genename="RN7SL1"
+
+##grabing gene info and sequence
+import httplib2
+import json
+
+try:
+ from urlparse import urlparse
+except ImportError:
+ from urllib.parse import urlparse
+
+headers = {
+ 'Accept': 'application/json',
+}
+
+uri = 'http://rest.genenames.org'
+path = '/fetch/symbol/'
+
+target = urlparse(uri+path+genename)
+method = 'GET'
+body = ''
+
+h = httplib2.Http()
+
+response, content = h.request(
+ target.geturl(),
+ method,
+ body,
+ headers)
+
+if response['status'] == '200':
+ data = json.loads(content)
+ accnumstr = str(data['response']['docs'][0]['refseq_accession'])
+ refnum = accnumstr[3:-2]
+ 
+else:
+ print 'Error detected: ' + response['status']
+
+##using refseq number to get sequence
+import urllib2
+url2 = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id="
+urlsetting = "&rettype=fasta&retmode=text"
+target2 = (url2+refnum+urlsetting)
+resp = urllib2.urlopen(target2)
+page = resp.read()
+page2 = str(page.split("\n",1)[1:])
+page3 = page2[2:-2]
+geneseq = (page3.replace("\\n",""))
+print (geneseq)
+
+threeseq=geneseq[-100:]
+folded=RNA.fold(threeseq)
+foldtrack=folded[0]
+foldenergy=folded[1]
+
+##output to txt
+foldout = "/home/rf/Desktop/fold.txt"
+outfile = open(foldout, 'a')
+outfile.write(genename+"\t"+refnum+"\t"+foldtrack+"\t"+str(foldenergy)+"\n")
+outfile.close()
